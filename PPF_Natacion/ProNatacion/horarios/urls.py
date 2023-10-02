6e860b2d15ea0ec825dade1ad546d9239e148d8e
Martin Torres <103221160+MartinT2222@users.noreply.github.com>
2023-10-02 17:16:42 -0300

    empezando con clases
    seleccion de clases

--- PPF_Natacion/ProNatacion/horarios/urls.py
+++ PPF_Natacion/ProNatacion/horarios/urls.py
@@ -1,13 +1,12 @@
 # horarios/urls.py
 from django.urls import path
 from . import views
+from horarios.views import detalle_clase
 
 app_name = 'horarios'
 
 urlpatterns = [
     # Agrega aquí las URLs para los horarios y clases
-    path('clases/', views.lista_clases, name='lista_clases'),
-    path('clases/<int:clase_id>/', views.detalle_clase, name='detalle_clase'),
-    path('horarios/', views.lista_horarios, name='lista_horarios'),
-    path('horarios/<int:horario_id>/', views.detalle_horario, name='detalle_horario'),
+    path('horarios/', views.listar_horarios_clase, name='listar_horarios_clase'),
+    path('clase/<int:clase_id>/', detalle_clase, name='detalle_clase'),
 ]
--- PPF_Natacion/ProNatacion/horarios/views.py
+++ PPF_Natacion/ProNatacion/horarios/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Clase, HorarioClase
 
+# Vista para mostrar todos los horarios de clase
+def listar_horarios_clase(request):
+    horarios = HorarioClase.objects.all()
+    horarios_list = [{'id': horario.horario_id,
+                      'dia_semana': horario.dia_semana,
+                      'hora_inicio': horario.hora_inicio.strftime('%H:%M'),
+                      'hora_fin': horario.hora_fin.strftime('%H:%M')} for horario in horarios]
+    return JsonResponse({'horarios': horarios_list})
 
-def lista_clases(request):
-    # Obtener todas las clases
-    clases = Clase.objects.all()
-    return render(request, 'horarios/lista_clases.html', {'clases': clases})
+# Vista para mostrar detalles de una clase y sus horarios
 def detalle_clase(request, clase_id):
-    # Obtener una clase por su ID
     clase = get_object_or_404(Clase, pk=clase_id)
-    # Obtener los horarios de esa clase
     horarios = HorarioClase.objects.filter(clase=clase)
     return render(request, 'horarios/detalle_clase.html', {'clase': clase, 'horarios': horarios})
-
 
 
 
